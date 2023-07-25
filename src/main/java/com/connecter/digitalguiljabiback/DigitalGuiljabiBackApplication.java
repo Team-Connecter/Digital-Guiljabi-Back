fb0f20a5e9449e437330994eac7f4a11addc66ad
Juhyorim <joohr1234@gmail.com>
2023-07-25 20:21:32 +0900

    initial commit

---
+++ src/main/java/com/connecter/digitalguiljabiback/DigitalGuiljabiBackApplication.java
@@ -0,0 +1,13 @@
+package com.connecter.digitalguiljabiback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalGuiljabiBackApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DigitalGuiljabiBackApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/connecter/digitalguiljabiback/DigitalGuiljabiBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connecter.digitalguiljabiback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalGuiljabiBackApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
