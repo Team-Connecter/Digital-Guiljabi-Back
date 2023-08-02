7ea07779215309fd498190edafcd06f4cdf3e9dd
Juhyorim <joohr1234@gmail.com>
2023-08-03 03:37:39 +0900

    test: 정보글 삭제 테스트 정보글 삭제, 자기자신만 삭제가능 테스트

--- src/test/java/com/connecter/digitalguiljabiback/service/BoardServiceTest.java
+++ src/test/java/com/connecter/digitalguiljabiback/service/BoardServiceTest.java
@@ -7,6 +7,7 @@ import com.connecter.digitalguiljabiback.domain.Users;
 import com.connecter.digitalguiljabiback.dto.board.CardDto;
 import com.connecter.digitalguiljabiback.dto.board.request.AddBoardRequest;
 import com.connecter.digitalguiljabiback.dto.board.response.BoardResponse;
+import com.connecter.digitalguiljabiback.exception.ForbiddenException;
 import com.connecter.digitalguiljabiback.repository.UserRepository;
 import org.junit.jupiter.api.*;
 import org.slf4j.Logger;
@@ -16,6 +17,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -54,7 +56,7 @@ class BoardServiceTest {
       sources[i] = "source" + i;
   }
 
-  @DisplayName("게시글 생성") //게시글 생성 시 엔티티가 잘 만들어지는지, get했을 때도 잘 불러와지는지 확인
+  @DisplayName("정보글 생성") //정보글 생성 시 엔티티가 잘 만들어지는지, get했을 때도 잘 불러와지는지 확인
   @Transactional
   @Order(1)
   @Test
@@ -98,4 +100,48 @@ class BoardServiceTest {
       assertEquals(cards[i].getSubTitle(), getCard.get(i).getSubTitle());
     }
   }
+
+  //정보글 삭제: 자기 자신만 가능하게
+  @DisplayName("정보글 삭제")
+  @Transactional
+  @Order(2)
+  @Test
+  void deleteBoard() {
+    Users user1 = Users.makeUsers("KAKAO12341", "asdf", OauthType.KAKAO);
+    user1 = userRepository.save(user1);
+    Users user2 = Users.makeUsers("KAKAO12342", "asdf", OauthType.KAKAO);
+    user2 = userRepository.save(user2);
+
+    AddBoardRequest request = new AddBoardRequest(title,introduction,thumbnail,cards,tags, sources);
+
+    Board newBoard = boardService.makeBoard(user1, request);
+
+    Users finalUser = user2;
+    assertThrows(ForbiddenException.class, () -> {
+      boardService.deleteBoard(finalUser, newBoard.getPk());
+    });
+
+    boardService.deleteBoard(user1, newBoard.getPk());
+
+    Users finalUser1 = user1;
+    assertThrows(NoSuchElementException.class, () -> {
+      boardService.getBoardInfo(newBoard.getPk(), finalUser1);
+    });
+  }
+
+  //정보글 승인 + 카테고리 등록 + 조회 확인
+
+  //정보글 승인거부
+
+  //정보글 검색 잘되는지 확인
+
+  //정보글 수정
+
+
+
+
+
+
+
+
 }
---
+++ src/test/java/com/connecter/digitalguiljabiback/service/CategoryServiceTest.java
@@ -0,0 +1,17 @@
+package com.connecter.digitalguiljabiback.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryServiceTest {
+  
+  //카테고리 등록 테스트(조상)
+  
+  //카테고리 부모 밑에 등록 + 부모조회
+  
+  //카테고리 수정 테스트
+  
+  //카테고리 삭제 테스트
+  
+  //카테고리 이등 테스트
+
+}
