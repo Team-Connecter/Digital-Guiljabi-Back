e8cebe8c74f77253fc1cf7b675dd717a5f76f64b
Juhyorim <joohr1234@gmail.com>
2023-08-11 03:17:48 +0900

    Fix: 승인되지 않은 정보글 조회 dto 안들어가는 오류 수정
    승인되지 않은 정보글 조회 dto 안들어가는 오류 수정 @ModelAttrivute -> @RequestParam
    swagger 문서 추가

--- src/main/java/com/connecter/digitalguiljabiback/dto/board/request/BoardListRequest.java
+++ src/main/java/com/connecter/digitalguiljabiback/dto/board/request/BoardListRequest.java
@@ -2,22 +2,26 @@ package com.connecter.digitalguiljabiback.dto.board.request;
 
 import com.connecter.digitalguiljabiback.dto.board.SortType;
 import jakarta.validation.constraints.Min;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 
 @Getter
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class BoardListRequest {
   //전부 nullable
   private Long categoryPk;
   private String q;
+  @Builder.Default
   @Min(value = 2, message = "page 크기는 1보다 커야합니다")
-  private Integer pageSize;
+  private Integer pageSize = 10;
+  @Builder.Default
   @Min(value = 1, message = "page는 0보다 커야합니다")
-  private Integer page;
+  private Integer page = 1;
   private SortType sort;
+
+  public static BoardListRequest makeRequest(Long categoryPk, String q, Integer pageSize, Integer page, SortType sort) {
+    return new BoardListRequest(categoryPk, q, pageSize, page, sort);
+  }
 }
