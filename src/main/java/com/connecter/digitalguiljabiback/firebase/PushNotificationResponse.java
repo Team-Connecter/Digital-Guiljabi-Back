4337fa371da368b2763cb4bb8753af3ee3448d49
eunslog <yeseo235@gmail.com>
2023-08-13 03:19:48 +0900

    feat/FirebaseNotification

---
+++ src/main/java/com/connecter/digitalguiljabiback/firebase/PushNotificationResponse.java
@@ -0,0 +1,15 @@
+package com.connecter.digitalguiljabiback.firebase;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class PushNotificationResponse {
+    private int status;
+    private String message;
+}
