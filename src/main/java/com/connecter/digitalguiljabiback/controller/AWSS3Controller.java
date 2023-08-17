4b2aea4205b472c8079a66b5e3b351f698561448
eunslog <yeseo235@gmail.com>
2023-08-17 17:46:24 +0900

    feat/awsS3

--- src/main/java/com/connecter/digitalguiljabiback/controller/AWSS3Controller.java
+++ src/main/java/com/connecter/digitalguiljabiback/controller/AWSS3Controller.java
@@ -1,2 +1,42 @@
-package com.connecter.digitalguiljabiback.controller;public class AWSS3Controller {
+package com.connecter.digitalguiljabiback.controller;
+
+import com.connecter.digitalguiljabiback.service.AWSS3Service;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@Tag(name = "AWS S3", description = "AWS S3 관련 API")
+@RestController
+@Slf4j
+@RequestMapping("/api/v1")
+public class AWSS3Controller {
+
+    private final AWSS3Service awsS3Service;
+
+    @Autowired
+    public AWSS3Controller(AWSS3Service awsS3Service) {
+        this.awsS3Service = awsS3Service;
+    }
+
+    @Operation(summary = "사진 업로드", description = """
+    201: 성공<br>
+    """)
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("filePath") MultipartFile file) {
+        try {
+            awsS3Service.uploadFileToFolder(file.getOriginalFilename(), file.getInputStream());
+            return ResponseEntity.ok("File uploaded successfully.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to upload file.");
+        }
+    }
 }
+
--- src/main/java/com/connecter/digitalguiljabiback/service/AWSS3Service.java
+++ src/main/java/com/connecter/digitalguiljabiback/service/AWSS3Service.java
@@ -1,2 +1,75 @@
-package com.connecter.digitalguiljabiback.service;public class AWSS3Service {
+package com.connecter.digitalguiljabiback.service;
+
+import com.amazonaws.SdkClientException;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.*;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class AWSS3Service {
+
+    private final AmazonS3 s3;
+    private final String bucketName = "digital-guiljabi";
+
+    @Value("${aws.s3.accessKey}")
+    private String accessKey ;
+
+    @Value("${aws.s3.secretKey}")
+    private String secretKey;
+
+    public AWSS3Service() {
+        try{
+        final String endPoint = "https://kr.object.ncloudstorage.com";
+        final String regionName = "kr-standard";
+
+        // S3 client initialization
+        s3 = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endPoint, regionName))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accessKey, secretKey)))
+                .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to initialize AWSS3Service", e);
+        }
+    }
+
+    public void uploadFileToFolder(String objectName, InputStream filePath) {
+
+        String folderName = "sample/";
+        // Upload local file to the specified folder
+        String key = folderName + objectName;
+
+        try {
+//            s3.putObject(bucketName, key, new File(filePath));
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType("image/jpeg"); // 이미지 타입에 맞게 설정
+
+            s3.putObject(bucketName, key, filePath, metadata);
+            System.out.format("Object %s has been uploaded to folder %s.\n", objectName, folderName);
+
+            // Set bucket ACL
+            AccessControlList bucketAcl = s3.getBucketAcl(bucketName);
+            bucketAcl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+            s3.setBucketAcl(bucketName, bucketAcl);
+
+            // Set object ACL
+            AccessControlList objectAcl = s3.getObjectAcl(bucketName, key);
+            objectAcl.grantPermission(GroupGrantee.AllUsers, Permission.Read);
+            s3.setObjectAcl(bucketName, key, objectAcl);
+
+
+        } catch (AmazonS3Exception e) {
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            e.printStackTrace();
+        }
+    }
 }
